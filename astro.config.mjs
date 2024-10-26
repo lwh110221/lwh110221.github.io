d700e1900fa190c7d6a76242ca477838fc17f045
lwh110221 <2285813721@qq.com>
2024-10-27 00:00:58 +0800

    fix2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+	site: 'https://lwh110221.github.io', 
+  	integrations: [starlight()],
 	integrations: [
 		starlight({
 			title: 'Ahao的知识库',
