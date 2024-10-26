518c3ab99571d6054dc4eedd387ff64cf263bf6b
lwh110221 <2285813721@qq.com>
2024-10-26 20:45:44 +0800

    init starlight

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Ahao的知识库',
+			locales: {
+				root: {
+				  label: '简体中文',
+				  lang: 'zh-CN',
+				},
+			},
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: '目录',
+					items: [
+						{ label: '示范', slug: 'guides/example' },
+					],
+				},
+
+			],
+		}),
+	],
+});
