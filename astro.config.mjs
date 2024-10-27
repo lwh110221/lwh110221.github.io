34479d05db0545ff282a8d39b98fd921cb989634
lwh110221 <2285813721@qq.com>
2024-10-27 20:44:02 +0800

    10/27-pull

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,9 +23,24 @@ export default defineConfig({
 					],
 				},
 				{
+					label: '考点大纲',
+					items: [
+						{ label: '计算机组成原理', slug: '25dagang/大纲提取-计算机组成原理' },
+						{ label: '数据结构', slug: '25dagang/大纲提取-数据结构' },
+						{ label: '计算机网络', slug: '25dagang/大纲提取-计算机网络' },
+						{ label: '操作系统', slug: '25dagang/大纲提取-操作系统' },
+						{ label: '数据库系统', slug: '25dagang/大纲提取-数据库系统' },
+						{ label: '通信原理（信号与系统）', slug: '25dagang/大纲提取-通信原理' },
+						{ label: '通信网理论基础', slug: '25dagang/大纲提取-通信网基础' },
+						{ label: '光纤通信', slug: '25dagang/大纲提取-光纤通信' },
+						{ label: '数字化新技术理论', slug: '25dagang/大纲提取-数字化新技术' },
+					],
+				},
+				{
 					label: '计算机组成原理',
 					items: [
-						{ label: '考点汇总', slug: 'jizu/kaodian' },
+						{ label: '知识点-整理版', slug: 'jizu/计组-整理' },
+						{ label: '知识点-整理版2', slug: 'jizu/计组-整理2' },						
 					],
 				},
 				{
